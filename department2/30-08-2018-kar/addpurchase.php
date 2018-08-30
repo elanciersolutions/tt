13377171691dc92f18abb5babcc1111adfd1e160
karthielan <42829186+karthielan@users.noreply.github.com>
2018-08-30 12:54:46 +0530

    purchase dynamic remove
    footer jquery on click function

---
+++ department2/30-08-2018-kar/addpurchase.php
@@ -0,0 +1,208 @@
+<?php 
+$title="Purchase Entry";
+include("header.php");
+extract($_GET);
+if(isset($_POST['submit'])){
+	extract($_POST);
+	for($i=0; $i<count($pname);$i++)
+	{
+    
+	 $select_product = mysql_query("SELECT * FROM addpurchase WHERE `product_name`='$pname[$i]' AND `category_name`='$category' ");		
+	
+	$in = mysql_query("INSERT INTO `addpurchase` (`category_name`,`product_name`,`subcategory`,`price`,`commission`,`purchase_date`,`dtime`,`stock`,`dept`,`gst`,`brandname`) VALUES ('$category','$pname[$i]','$subname[$i]','$price[$i]','$commission[$i]','".date('Y-m-d')."','".date('Y-m-d H:i:s')."','$stock[$i]','$dept','$gst[$i]','$brandname[$i]')");
+	}
+	if($in)
+	{
+	echo "<script type='text/javascript'>swal({
+		title: 'Insert Successfully!...',
+		type: 'success'
+	}, function() {
+		window.location = 'purchase.php';
+	})</script>";
+	}
+}
+if(isset($_POST['update'])){
+	extract($_POST);
+	$in=mysql_query("UPDATE `addpurchase` SET `category_name`='$category',`subcategory`='$sub_categoryname',`product_name`='$product_name[1]',`brandname`='$brandname',`price`='$price',`commission`='$commission',`gst`='$gst',`stock`='$stock' WHERE `id`='$id'");
+	if($in){
+		echo "<script type='text/javascript'>swal({
+            title: 'Update Successfully!...',
+            type: 'success'
+        }, function() {
+            window.location = 'purchase.php';
+        })</script>";
+	} 
+}
+?>
+	<section id="main-content">
+		<section class="wrapper">
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<section class="panel">
+                        <header class="panel-heading">
+							<h4><?php if($_GET['id']!=''){ echo "Update"; } else{ echo "Add"; } ?>  Purchase</h4>
+							<div class="xtra_btn_top">
+								<a title="BACK" data-placement="top" data-toggle="tooltip" class="btn btn-danger tooltips" onclick="window.history.go(-1); return false;">Back</a>
+							</div>
+                        </header>
+                        <div class="panel-body">
+							<?php 
+							$dept1 = sessionvalue('department',$dept);
+						    $sql=mysql_fetch_array(mysql_query("SELECT * FROM `addpurchase` WHERE id='$id'")); 
+							extract($sql); $brandid = $sql['brandname']; $product_name = $sql['product_name']; $purid=$sql['id']; ?>
+                            <form class="cmxform form-horizontal tasi-form" id="product" method="post" name="product" enctype="multipart/form-data" >
+                             
+								  <div class="form-group">
+									<label class="col-lg-3 col-md-4 col-sm-4 col-xs-12 control-label" for="department">Category <span class="red">*</span></label>
+									<div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+										
+										<input type="text" name="category" id="category" value="<?php $departmen = mysql_fetch_array(mysql_query("SELECT * FROM department WHERE id='$dept1'")); echo  $departmen['department'];?>" class="form-control" autocomplete="off" readonly />
+									</div>
+									
+								<label class="col-lg-3 col-md-4 col-sm-4 col-xs-12 control-label" for="pname">Sub Category <span class="red">*</span></label>
+									<div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+									
+                                        <select name="sub_categoryname" id="sub_categoryname" class="form-control"   onchange="brand_fetch(this.value,<?php echo $dept1;?>,category.value )">
+										<option value="">--- Select ---</option>
+											<?php $ca=mysql_query("SELECT * FROM `sub_category` WHERE `dept`='$dept1' AND `status`=1");
+											while($cat=mysql_fetch_array($ca)){ ?>
+											<option value="<?php echo $cat['id'];?>" <?php if($cat['id'] == $subcategory){echo "selected";}?>><?php echo $cat['sub_categoryname'];?></option>
+											<?php }	
+											?>
+										</select>
+									</div>
+								</div>
+								<div class="form-group">
+									<label class="col-lg-3 col-md-4 col-sm-4 col-xs-12 control-label" for="price">Brand Name <span class="red">*</span></label>
+									<div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                                       
+										
+										 <select name="brandname" id="brandname" class="form-control" onchange="product_expand(this.value,<?php echo $dept1;?>,sub_categoryname.value)" >
+										 <option value="">--- Select ---</option>
+										 
+										 <?php 
+										    if($brandid != "")
+											{
+										   $ca=mysql_query("SELECT * FROM `brand` WHERE  `dept`='$dept' AND `id`='$brandid' AND `status`=1");
+											while($cat=mysql_fetch_array($ca)){ ?>
+											<option value="<?php echo $cat['id'];?>" <?php if($cat['id'] == $brandid){echo "selected";}?>><?php echo $cat['brandname'];?></option>
+											<?php }
+											}
+											
+?>
+										
+										 </select>
+										
+										
+									</div>
+									
+									<label class="col-lg-3 col-md-4 col-sm-4 col-xs-12 control-label" for="commission">Product Name <span class="red">*</span></label>
+									<div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                                      
+									    <select name="product_name" id="product_name" class="form-control" onchange="product_price(this.value,<?php echo $dept1;?>,sub_categoryname.value,brandname.value)" >
+										<option value="">--- Select ---</option>
+											<?php 
+										    if($product_name != "")
+											{
+										   $ca=mysql_query("SELECT * FROM `product` WHERE  `dept`='$dept' AND `pname`='$product_name' AND `status`=1");
+											while($cat=mysql_fetch_array($ca)){ ?>
+											<option value="<?php echo $cat['pname'];?>" <?php if($cat['pname'] == $product_name){echo "selected";}?>><?php echo $cat['pname'];?></option>
+											<?php }
+											}
+											
+?>
+										</select>
+									   
+									</div>
+									
+									
+								</div>
+								<div class="form-group">
+									<label class="col-lg-3 col-md-4 col-sm-4 col-xs-12 control-label" for="price">Price <span class="red">*</span></label>
+									<div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                                        <input  type="number" name="price" id="price" value="<?php echo $price;?>" class="form-control" autocomplete="off" readonly />
+									</div>
+									
+									<label class="col-lg-3 col-md-4 col-sm-4 col-xs-12 control-label" for="commission">Cash Pack <span class="red">*</span></label>
+									<div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                                        <input type="number" name="commission" id="commission" value="<?php echo $commission;?>" class="form-control" autocomplete="off" readonly />
+									</div>
+								</div>
+								<div class="form-group">
+									<label class="col-lg-3 col-md-4 col-sm-4 col-xs-12 control-label" for="gst">GST (%)<span class="red">*</span></label>
+									<div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                                      <input type="text" name="gst" id="gst" value="<?php echo $gst;?>" class="form-control" autocomplete="off" readonly />
+									</div>
+								<label class="col-lg-3 col-md-4 col-sm-4 col-xs-12 control-label" for="commission">Stock <span class="red">*</span></label>
+									<div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                                        <input   type="number" name="stock" id="stock" value="<?php echo $stock;?>" class="form-control" autocomplete="off" />
+										
+									</div>
+									</div>
+									
+									<?php if($purid == "")
+									{
+										?>
+									<div class="form-group last-btn">
+                                    <div class="col-lg-offset-2 col-lg-10">
+                                       
+										<button name="button" class="btn btn-primary" type="button" onclick="dynamic_purchase(product_name.value,price.value,gst.value,commission.value,stock.value,tr_count.value,sub_categoryname.value,brandname.value)">Add More </button>
+										
+                                    </div>
+                                </div>
+								
+								<div id="table_div" style="display:none">
+								
+								<table class="display table table-bordered cf" id="dyntable">
+									<thead>
+									<input type="hidden" name="tr_count" id="tr_count" value="1">
+										<tr>
+											<th>S.No</th>
+											<th>Product Name</th>
+											<th>Price</th>
+											<th>GST</th>
+											<th>Commission</th>
+											<th>Stock</th>
+											<th>Action</th>
+										</tr>
+									</thead>
+									<tbody id="product_tobody">
+									</tbody>
+									</table>
+								<div class="form-group last-btn">
+                                    <div class="col-lg-offset-2 col-lg-10">
+                                        <?php if($_GET['id']!=''){ ?>
+										<button name="update" class="btn btn-primary" type="submit">Save Changes </button>
+										<?php } else { ?>
+										<button name="submit" class="btn btn-primary" type="submit">Submit </button>
+										<?php } ?>
+                                    </div>
+                                </div>
+								</div>
+								<?php 
+									}
+									else{
+										?>
+										<div class="form-group last-btn">
+                                    <div class="col-lg-offset-2 col-lg-10">
+                                        
+										<button name="update" class="btn btn-primary" type="submit">Save Changes </button>
+										
+                                    </div>
+                                </div>
+										<?php
+									}
+									?>
+                            </form>
+                        </div>
+						<footer class="panel-footer">
+							<div class="xtra_btn_bottom">
+								<a title="BACK" onclick="window.history.go(-1); return false;" data-placement="top" data-toggle="tooltip" class="btn btn-danger tooltips" href="product_list.php">Back</a>
+							</div>
+						</footer>
+                    </section>
+                </div>
+            </div>
+		</section>
+	</section>
+<?php include("footer.php");?>
---
+++ department2/30-08-2018-kar/footer.php
@@ -0,0 +1,276 @@
+		<footer class="site-footer">
+			<div class="">
+				<div style="margin-left: 20%;float:left">
+				2018 &copy; Do First
+				</div>
+				
+				<span id="scrolltop"><i class="fa-angle-up"></i></span>
+				
+				<div style="float:right;">
+				<!--Design by<a href="http://elancier.com/" target="_blank" style="color:#fff"> elancier</a>-->
+				</div>			
+				
+			</div>
+		</footer>
+	</section>
+    <script src="<?php echo $adminurl;?>scripts/jquery-1.8.2.min.js"></script>
+	<script src="<?php echo $adminurl;?>vendors/bootstrap/js/bootstrap.min.js"></script>
+	
+	<!-- Left Sidebar -->
+    <script class="include" src="<?php echo $adminurl;?>scripts/jquery.dcjqaccordion.2.7.js"></script>
+	
+	<!-- Scroll -->
+    <script src="<?php echo $adminurl;?>vendors/nicescroll/jquery.scrollTo.min.js"></script>
+    <script src="<?php echo $adminurl;?>vendors/nicescroll/jquery.nicescroll.js" type="text/javascript"></script>
+	
+	<!-- Tags Input -->
+	<script src="<?php echo $adminurl;?>vendors/jquery-tags-input/jquery.tagsinput.min.js"></script>
+	
+	<!-- Input Mask -->
+	<script src="<?php echo $adminurl;?>vendors/bootstrap-inputmask/bootstrap-inputmask.min.js"></script>
+	
+	<!-- Switch Checkbox and Radios -->
+	<script src="<?php echo $adminurl;?>vendors/bootstrap-switch/js/bootstrap-switch.min.js"></script> 
+	
+	<!-- Ckeditor -->
+	<script src="<?php echo $adminurl;?>vendors/ckeditor/ckeditor.js"></script>
+	
+	<!-- Ckeditor -->
+	<script src="<?php echo $adminurl;?>vendors/bootstrap-fileupload/bootstrap-fileupload.min.js"></script>
+	
+	<!-- Date-Picker -->
+	<script src="<?php echo $adminurl;?>vendors/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
+	
+	<!-- Time-Picker -->
+	<script src="<?php echo $adminurl;?>vendors/bootstrap-timepicker/js/bootstrap-timepicker.min.js"></script>
+	
+	<!-- DateTime-Picker -->
+	<script src="<?php echo $adminurl;?>vendors/bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js"></script>
+	
+	<!-- DateRange-Picker -->
+	<script src="<?php echo $adminurl;?>vendors/bootstrap-daterangepicker/moment.min.js"></script>
+	<script src="<?php echo $adminurl;?>vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+	
+	<!-- Image Cropping -->
+	<!--<script src="vendors/jcrop/js/jquery.jcrop.min.js"></script>-->
+	<script src="<?php echo $adminurl;?>scripts/form-image-crop.js"></script>
+	
+	<!-- Advanced Datatable -->
+    <script src="<?php echo $adminurl;?>vendors/advanced-datatable/js/jquery.dataTables.min.js"></script>
+	
+	<!-- Editable Datatable - -->
+    <script src="<?php echo $adminurl;?>vendors/data-tables/js/datatable_tools.min.js"></script>
+    <script src="<?php echo $adminurl;?>scripts/editable-table.js"></script>
+		
+	<!-- Charts -->
+    <script src="<?php echo $adminurl;?>vendors/jquery-easy-pie-chart/js/jquery.easy-pie-chart.js"></script>
+    <script src="<?php echo $adminurl;?>scripts/jquery.sparkline.js"></script>
+    <script src="<?php echo $adminurl;?>scripts/sparkline-chart.js"></script>
+	
+	<!-- Validate Form -->
+    <script src="<?php echo $adminurl;?>scripts/jquery.validate.min.js"></script>
+	
+	<!-- Custom Script -->
+	<script src="<?php echo $adminurl;?>scripts/custom.js"></script>
+	
+	<script>
+		jQuery(document).ready(function () {
+			if (jQuery('.wrapper').width() < 1024) {
+				jQuery('#width_800').removeAttr('disabled');
+				var rows1 = jQuery("table.display tbody tr td");
+				var rows2 = jQuery("table.display thead tr th");
+				rows1.each(function () {
+					jQuery(rows2[jQuery(this).index()]).height(jQuery(this).height());
+				});
+			} else {
+				jQuery('#width_800').attr('disabled', 'disabled');
+				jQuery('table.display thead tr th, table tbody tr td').css({
+					'height': ''
+				});
+			}
+			if (jQuery('#container').hasClass('sidebar-closed')) {
+				jQuery('#sidebar').css({ 'margin-left': '-210px' });
+				jQuery('#sidebar > ul').hide();
+			} else {
+				jQuery('#main-content').css({ 'margin-left': '0px' });
+				jQuery('#sidebar > ul').show();
+			}
+		});
+		jQuery(window).resize(function () {
+			var footer_height = jQuery('.site-footer').height() + 16 + 'px';
+			jQuery('#main-content').css({
+				'padding-bottom': footer_height
+			});
+			if (jQuery('.wrapper').width() < 1024) {
+				jQuery('#width_800').removeAttr('disabled');
+				var rows1 = jQuery("table.display tbody tr td");
+				var rows2 = jQuery("table.display thead tr th");
+				rows1.each(function () {
+					jQuery(rows2[jQuery(this).index()]).height(jQuery(this).height());
+				});
+			} else {
+				jQuery('#width_800').attr('disabled', 'disabled');
+				jQuery('table.display thead tr th, table tbody tr td').css({
+					'height': ''
+				});
+			}
+			if (jQuery('#container').hasClass('sidebar-closed')) {
+				jQuery('#sidebar').css({ 'margin-left': '-210px' });
+				jQuery('#sidebar > ul').hide();
+			} else {
+				jQuery('#main-content').css({ 'margin-left': '0px' });
+				jQuery('#sidebar > ul').show();
+			}
+		});
+		/***** Delete Confirmation *****/
+		function del() {
+			var retVal = confirm("Do you want to Delete the Record ?");
+			if (retVal == true) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		/********Coupon Generation confirmation**********/
+		function Confirm() {
+			var retVal = confirm("Do you want to generate coupon code for this User?");
+			if (retVal == true) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		
+	/*********** Find SubCategory  ***************/
+		
+		function subcategory(catid,dept)
+		{
+            
+          $.ajax({
+			    
+                url     : 'subcategory_find.php',
+                type    : 'POST',				
+				data    : {'catid':catid,'dept':dept},
+				
+				success : function(subid)
+				{
+					
+				  	$('#sub_categoryname').html(subid);
+				}
+		  });
+		  
+		}
+		/*********** Find Product Details  ***************/
+		function product_details(subid,dept,catid)
+		{
+			
+			 $.ajax({
+			    
+                url     : 'productdetails_find.php',
+                type    : 'POST',				
+				data    : {'subid':subid,'dept':dept,'catid':catid},
+				
+				success : function(product_details)
+				{
+					
+				  	$('#list_of_productcode').html(product_details);
+				}
+		  });
+		}
+		
+		function product_expand(brandid,dept,subid)
+		{
+			
+			 $.ajax({
+			    
+                url     : 'productexpand_find.php',
+                type    : 'POST',				
+				data    : {'brandid':brandid,'dept':dept,'subid':subid},
+				
+				success : function(products_details)
+				{
+					
+				  	$('#product_name').html(products_details);
+				}
+		  });
+		}
+		
+		/*********** Find Brand Name  ***************/
+		function brand_fetch(subid,dept,catid)
+		{
+			
+			$.ajax({
+			    
+                url     : 'brand_find.php',
+                type    : 'POST',				
+				data    : {'subid':subid,'dept':dept,'catid':catid},
+				
+				success : function(brand)
+				{
+				  	$('#brandname').html(brand);
+				}
+		  });
+		}
+		
+		function product_price(pid,dept,subid,brandid)
+		{
+			$.ajax({
+			    
+                url     : 'productprice_find.php',
+                type    : 'POST',				
+				data    : {'brandid':brandid,'dept':dept,'subid':subid,'pid':pid},
+				
+				success : function(product_price)
+				{
+					
+				    var product = product_price.split('@');	
+						
+				  	$('#price').val(product[0]);
+					$('#commission').val(product[1]);
+					$('#gst').val(product[2]);
+				}
+		  });
+		}
+		
+		/*********** Find Dynamic Purchase  ***************/
+		
+		function dynamic_purchase(pname,price,gst,commission,stock,tr_count,subname,brandname)
+		{
+			var product_name = $('#product_name').val();
+			
+			var pname = pname.split('&');
+			if(product_name != "")
+			{
+			
+			var data = '<tr id="row'+tr_count+'"><td>'+tr_count+'</td><td>'+pname[1]+'<input type="hidden" name="pname[]" value="'+pname[1]+'"></td><td>'+price+'<input type="hidden" name="price[]" value="'+price+'"></td><td>'+gst+'%<input type="hidden" name="gst[]" value="'+gst+'"></td><td>'+commission+'<input type="hidden" name="commission[]" value="'+commission+'"></td><td>'+stock+'<input type="hidden" name="stock[]" value="'+stock+'"><input type="hidden" name="subname[]" value="'+subname+'"><input type="hidden" name="brandname[]" value="'+brandname+'"></td><td><a  class="block_tip tooltips text-center" onclick="remove_tr('+tr_count+')"><i class="fa fa-times"></i></a></td></tr>';
+			
+			$('#table_div').css('display','block');
+			
+			var append_data = $('#product_tobody').append(data);
+			
+			var rowcount = parseInt(tr_count)+parseInt(1);
+			
+			$('#tr_count').val(rowcount);
+			
+			$('form#product')[0].reset();
+			}
+			else {
+				swal('please Enter the Product');
+				$('#sub_categoryname').focus();
+				return false;
+			}
+		}
+		
+		function remove_tr(tr_count)
+		{
+		   	var rem = confirm("Do you want to Remove the Purchase ?");
+			if(rem = 'true')
+			{
+			$('#row'+tr_count).remove();
+			}
+		}
+		
+		
+	</script>
+</body>
+</html>
