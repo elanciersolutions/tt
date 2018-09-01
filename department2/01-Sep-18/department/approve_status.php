5c33e5dc27fd049142a6dfec50982d39c984aeb5
karthielan <42829186+karthielan@users.noreply.github.com>
2018-09-01 12:30:41 +0530

    Particaily Sales Order Completed

---
+++ department2/01-Sep-18/department/approve_status.php
@@ -0,0 +1,53 @@
+<?php  include('../admin/connect/config.php');error_reporting(1);extract($_POST); 
+
+    if($saleorder_id != "")
+	 {
+
+     $select_approve = mysql_query("SELECT * FROM `sales` WHERE `saleorder_id`='$saleorder_id'");
+	 
+	 while($fetch_approve = mysql_fetch_array($select_approve))
+	 {
+		  $sale_qty = $fetch_approve['sale_qty'];
+		  $pid = $fetch_approve['product'];
+		  $sale_total = $fetch_approve['sale_total'];
+		  $total = $fetch_approve['total'];
+		  $qty = $fetch_approve['qty'];
+		  
+		  if($sale_qty != 0)
+		  {
+			  $approve_sale=mysql_query("UPDATE `sales` SET `approve_status`=1  WHERE  `cat`='$dept' AND `saleorder_id`='$saleorder_id' AND product='$pid'");
+		  }
+		  else if($sale_qty == 0 &&  $sale_total == "")
+		  {
+			 $approve_sale=mysql_query("UPDATE `sales` SET `approve_status`=1,sale_qty='$qty',sale_total='$total'  WHERE  `cat`='$dept' AND `saleorder_id`='$saleorder_id' AND product='$pid'"); 
+		  }
+	 }
+	 
+	  if($approve_sale)
+	 {
+		 echo 1;
+	 }
+	 else
+	 {
+		 echo 2;  
+	 }
+	 
+    /* 
+	$approve_sale=mysql_query("UPDATE `sales` SET `approve_status`=1  WHERE  `cat`='$dept' AND `saleorder_id`='$saleorder_id' ");
+	 
+	  if($approve_sale)
+	 {
+		 echo 1;
+	 }
+	 else
+	 {
+		 echo 2;  
+	 }
+	*/
+	}
+	
+	 
+											
+?>
+
+ 
