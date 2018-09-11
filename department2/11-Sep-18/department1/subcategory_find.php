60ae1c03cfc157e8a0dc669b66a6e5f3de3e7d6b
karthielan <42829186+karthielan@users.noreply.github.com>
2018-09-11 15:30:22 +0530

    lats

---
+++ department2/11-Sep-18/department1/subcategory_find.php
@@ -0,0 +1,10 @@
+<?php  include('../admin/connect/config.php');error_reporting(1);extract($_POST); 
+$catid = explode("@",$catid);
+?>
+
+<option value="">--- Select ---</option>
+									<?php $ca=mysql_query("SELECT * FROM `sub_category` WHERE catid='$catid[0]' AND `status`=1");
+									while($cat=mysql_fetch_array($ca)){ ?>
+									<option value="<?php echo $cat['id'];?>"><?php echo $cat['sub_categoryname'];?></option>
+									<?php }
+?>
